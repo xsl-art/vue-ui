06300941de3a99ad5137f0b8746e14ffefe5acc2
xsl <hello@xsl.com>
2026-05-07 16:53:35 +0800

    feat(Switch):新增语义化属性

--- src/components/Switch/Switch.vue
+++ src/components/Switch/Switch.vue
@@ -2,7 +2,8 @@
   <div class="vk-switch" :class="{ [`vk-switch--${size}`]: size, 'is-checked': checked, 'is-disabled': disabled }"
     @click="switchClick">
     <input type="checkbox" role="switch" ref="inputRef" class="vk-switch__input" :name="name" :disabled="disabled"
-      @keydown.enter="switchClick">
+      :aria-checked="checked" :aria-disabled="disabled" :aria-label="(checked ? activeText : inactiveText)"
+      @keydown.enter="switchClick" />
     <div class="vk-switch__core">
       <div class="vk-switch__core-inner">
         <span class="vk-switch__core-inner-text" v-if="activeText || inactiveText">
@@ -15,38 +16,43 @@
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted, ref, watch } from 'vue';
-import type { SwitchProps, SwitchEmits } from './types';
-
+import { computed, onMounted, ref, watch } from "vue";
+import type { SwitchProps, SwitchEmits } from "./types";
 
 defineOptions({
-  name: 'VkSwitch'
-})
+  name: "VkSwitch",
+});
 const props = withDefaults(defineProps<SwitchProps>(), {
-  size: 'default',
+  size: "default",
   activeValue: true,
-  inactiveValue: false
-})
+  inactiveValue: false,
+});
 
-const emits = defineEmits<SwitchEmits>()
-const inputRef = ref<HTMLInputElement>()
-const innerValue = ref(props.modelValue)
-const checked = computed(() => innerValue.value === props.activeValue)
+const emits = defineEmits<SwitchEmits>();
+const inputRef = ref<HTMLInputElement>();
+const innerValue = ref(props.modelValue);
+const checked = computed(() => innerValue.value === props.activeValue);
 
 const switchClick = () => {
-  if (props.disabled) return
-  const newValue = checked.value ? props.inactiveValue : props.activeValue
-  innerValue.value = newValue
-  emits('update:modelValue', newValue)
-  emits('change', newValue)
-}
+  if (props.disabled) return;
+  const newValue = checked.value ? props.inactiveValue : props.activeValue;
+  innerValue.value = newValue;
+  emits("update:modelValue", newValue);
+  emits("change", newValue);
+};
 
 onMounted(() => {
   //同步checkbox状态
-  inputRef.value!.checked = checked.value
-})
+  inputRef.value!.checked = checked.value;
+});
 
-watch(() => checked.value, (newVal) => inputRef.value!.checked = newVal)
+watch(
+  () => checked.value,
+  (newVal) => (inputRef.value!.checked = newVal),
+);
 
-watch(() => props.modelValue, (newVal) => innerValue.value = newVal)
+watch(
+  () => props.modelValue,
+  (newVal) => (innerValue.value = newVal),
+);
 </script>
