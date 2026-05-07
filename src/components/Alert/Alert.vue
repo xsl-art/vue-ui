68d2bb25906e0d5cc016df26882d8e854775080b
xsl <hello@xsl.com>
2026-05-07 14:25:29 +0800

    feat(Alert):新增语义化属性

--- src/components/Alert/Alert.vue
+++ src/components/Alert/Alert.vue
@@ -6,19 +6,20 @@
       'is-closable': closable,
       'is-center': center,
       'is-showIcon': icon
-    }" role="alert" v-if="visible">
+    }" role="alert" v-if="visible" :aria-labelledby="titleId" :aria-describedby="contentId">
       <div class="vk-alert__icon" v-if="icon">
-        <Icon :icon="typeIconMap[type!]" class="icon" />
+        <Icon :icon="typeIconMap[type!]" class="icon" aria-hidden="true" />
       </div>
       <div class="vk-alert__content">
-        <div class="vk-alert__title">
+        <div class="vk-alert__title" :id="titleId">
           <slot name="title">{{ title }}</slot>
         </div>
-        <div class="vk-alert__description">
+        <div class="vk-alert__description" :id="contentId">
           <slot name="content">{{ content }}</slot>
         </div>
       </div>
-      <div class="vk-alert__clear" v-if="closable" @click.stop="handleClick">
+      <div class="vk-alert__clear" v-if="closable" @click.stop="handleClick" @keydown.enter="handleClick" role="button"
+        tabindex="0" aria-label="Close alert">
         <Icon icon="xmark" />
       </div>
     </div>
@@ -44,6 +45,11 @@ const emits = defineEmits<AlertEmits>()
 const alertRef = ref<AlertInstance>()
 
 const visible = ref(true)
+
+//语义化
+const instanceId = Math.random().toString(36).slice(2, 8);
+const titleId = `alert-title-${instanceId}`;
+const contentId = `alert-content-${instanceId}`;
 
 //图标映射
 const typeIconMap: Record<string, string> = {
