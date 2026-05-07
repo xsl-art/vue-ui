cc0e8e36f09e4d67579373bdcb2318c302e2a477
xsl <hello@xsl.com>
2026-05-07 16:21:05 +0800

    feat(Input):新增语义化属性

--- src/components/Input/Input.vue
+++ src/components/Input/Input.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="vk-input" :class="{
     [`vk-input--${type}`]: type,
-    [`vk-input--${size}`]: size, 'is-disabled': disabled,
+    [`vk-input--${size}`]: size,
+    'is-disabled': disabled,
     'is-prepend': $slots.prepend,
     'is-prefix': $slots.prefix,
     'is-suffix': $slots.suffix,
     'is-append': $slots.append,
-    'is-focus': isFocus
+    'is-focus': isFocus,
   }">
     <!-- input -->
     <template v-if="type !== 'textarea'">
       <!-- prepend slot -->
-      <div class="vk-input__prepend" v-if="$slots.prepend">
+      <div class="vk-input__prepend" v-if="$slots.prepend" :id="prependId">
         <slot name="prepend"></slot>
       </div>
       <div class="vk-input__wrapper">
@@ -19,8 +20,8 @@
         <span class="vk-input__prefix" v-if="$slots.prefix">
           <slot name="prefix"></slot>
         </span>
-        <input class="vk-input__inner" ref="inputRef"
-          :type="showPassword ? passwordVisible ? 'text' : 'password' : type" v-bind="$attrs" :disabled="disabled"
+        <input class="vk-input__inner" ref="inputRef" :id="inputIdFromFormItem"
+          :type="showPassword ? (passwordVisible ? 'text' : 'password') : type" v-bind="$attrs" :disabled="disabled"
           :readonly="readonly" :autocomplete="autocomplete" :placeholder="placeholder" :autofocus="autofocus"
           :form="form" v-model="innerValue" @change="handleChange" @input="handleInput" @blur="handleBlur"
           @focus="handleFocus" />
@@ -37,7 +38,7 @@
         </span>
       </div>
       <!-- append slot -->
-      <div class="vk-input__append" v-if="$slots.append">
+      <div class="vk-input__append" v-if="$slots.append" :id="appendId">
         <slot name="append"></slot>
       </div>
     </template>
@@ -52,82 +53,94 @@
 </template>
 
 <script setup lang="ts">
-import { computed, inject, nextTick, ref, watch, type Ref } from 'vue';
-import Icon from '../Icon/Icon.vue';
-import type { InputEmits, InputProps } from './types';
-import { formItemContextKey } from '../Form/types';
+import { computed, inject, nextTick, ref, watch, type Ref } from "vue";
+import Icon from "../Icon/Icon.vue";
+import type { InputEmits, InputProps } from "./types";
+import { formItemContextKey } from "../Form/types";
 
 defineOptions({
-  name: 'VkInput',
-  inheritAttrs: false
-})
+  name: "VkInput",
+  inheritAttrs: false,
+});
 
 const props = withDefaults(defineProps<InputProps>(), {
-  autocomplete: 'off',
-  rows: 2
-})
+  autocomplete: "off",
+  rows: 2,
+});
 
-const emits = defineEmits<InputEmits>()
+const emits = defineEmits<InputEmits>();
 
-const inputRef = ref() as Ref<HTMLInputElement | HTMLTextAreaElement>
-const isFocus = ref(false)
-const innerValue = ref(props.modelValue ?? '')
-const passwordVisible = ref(false)
-const showClear = computed(() => props.clearable && !props.disabled && !!innerValue.value && isFocus.value)
-const showPasswordArea = computed(() => props.showPassword && !props.disabled && !!innerValue.value)
+const inputRef = ref() as Ref<HTMLInputElement | HTMLTextAreaElement>;
+const isFocus = ref(false);
+const innerValue = ref(props.modelValue ?? "");
+const passwordVisible = ref(false);
+const showClear = computed(
+  () => props.clearable && !props.disabled && !!innerValue.value && isFocus.value,
+);
+const showPasswordArea = computed(
+  () => props.showPassword && !props.disabled && !!innerValue.value,
+);
 
 //校验
-const formItemContext = inject(formItemContextKey, undefined)
+const formItemContext = inject(formItemContextKey, undefined);
+const inputIdFromFormItem = computed(() => formItemContext?.inputId);
+
+const prependId = `vk-input-prepend-${Math.random().toString(36).slice(2, 8)}`;
+const appendId = `vk-input-append-${Math.random().toString(36).slice(2, 8)}`;
+
 const runValidate = (trigger?: string) => {
   if (formItemContext) {
-    formItemContext?.validate(trigger).catch((e) => console.error(e.errors))
+    formItemContext?.validate(trigger).catch((e) => console.error(e.errors));
   }
-}
+};
 const keepFocus = async () => {
-  await nextTick()
-  inputRef.value!.focus()
-}
+  await nextTick();
+  inputRef.value!.focus();
+};
 
 const handleChange = () => {
-  emits('change', innerValue.value)
-  runValidate('change')
-}
+  emits("change", innerValue.value);
+  runValidate("change");
+};
 
 const handleInput = () => {
-  emits('update:modelValue', innerValue.value)
-  emits('input', innerValue.value)
-  runValidate('input')
-}
+  emits("update:modelValue", innerValue.value);
+  emits("input", innerValue.value);
+  runValidate("input");
+};
 
 const handleFocus = (event: FocusEvent) => {
-  isFocus.value = true
-  emits('focus', event)
-}
+  isFocus.value = true;
+  emits("focus", event);
+};
 
 const handleBlur = (event: FocusEvent) => {
-  isFocus.value = false
-  emits('blur', event)
-  runValidate('blur')
-}
+  isFocus.value = false;
+  emits("blur", event);
+  runValidate("blur");
+};
 
 const handleClear = () => {
-  innerValue.value = ''
-  emits('clear')
-  emits('update:modelValue', '')
-  emits('change', '')
-}
+  innerValue.value = "";
+  emits("clear");
+  emits("update:modelValue", "");
+  emits("change", "");
+};
 
 const togglePasswordVisible = () => {
-  passwordVisible.value = !passwordVisible.value
-}
+  passwordVisible.value = !passwordVisible.value;
+};
 
-const NOOP = () => { }
+const NOOP = () => { };
 
-watch(() => props.modelValue, (newValue) => {
-  innerValue.value = newValue ?? ''
-})
+watch(
+  () => props.modelValue,
+  (newValue) => {
+    innerValue.value = newValue ?? "";
+  },
+);
 
 defineExpose({
-  ref: inputRef
-})
+  ref: inputRef,
+});
 </script>
